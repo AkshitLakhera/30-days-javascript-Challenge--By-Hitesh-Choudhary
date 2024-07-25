f4f9d519dcd5700cb86939c0fe99ca1e81774c71
“[Akshit <akshitlakhera14@gmail.com>
2024-07-25 22:18:39 +0530

    Challenge 11 questions added

--- Day11/Challenge11.js
+++ Day11/Challenge11.js
@@ -75,3 +75,64 @@ const secondAsyncOperation = (previousResult) => {
     }
   }
   executePromise();
+
+  //Activity 3 is done as I have already used modern technique async await rather then catch
+
+  //Activity 4 Fetching data from api
+  fetch("https://api.tvmaze.com/search/shows?q=girls",)
+  .then ( response =>{
+    if(!response.ok){
+        console.error("the erro came ",response.statusText)
+    }
+    // Parse the JSON data
+    return response.json();
+  })
+  .then(data => {
+    console.log(data)
+  }).catch(error => {
+    // Handle any errors
+    console.error('Error fetching data:', error);
+  });
+//using async await
+
+const fetchData  = async () =>{
+    try {
+        const apiEndpoint = 'https://api.tvmaze.com/search/shows?q=girls';
+        const response = await fetch(apiEndpoint);
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+}
+//Promis.all :It return value when all the promise get resolved
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+//Promise .race  first complete first win
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Promise 4 resolved after 2 seconds');
+  }, 2000);
+});
+const promise5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Promise 2 resolved after 1 second');
+  }, 1000);
+});
+Promise.race([promise1, promise2])
+  .then((result) => {
+    console.log(result); // "Promise 2 resolved after 1 second"
+  })
+  .catch((error) => {
+    console.error(error);
+  });
